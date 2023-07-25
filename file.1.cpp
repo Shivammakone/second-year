8fed028f01c14bf54cb918f9a7f5d36607673310
Shivammakone <137860523+Shivammakone@users.noreply.github.com>
2023-07-25 19:07:30 +0530

    Add files via upload

---
+++ file.1.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<ctype.h>
+#include<stdlib.h>
+int main()
+{
+	FILE *fp1;
+	int num;
+	fp1=fopen("number.txt","r");
+	if(fp1==NULL)
+	{
+		printf("file not found");
+		exit(0);
+	}
+	while(!feof(fp1))
+	{
+		fscanf(fp1,"%d",&num);
+		printf("%d\n",num);
+	}
+	fclose(fp1);
+}
